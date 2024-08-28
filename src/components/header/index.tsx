f945c51690c5b69c36d1d8f580ad642426793dc7
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-08-28 17:36:23 -0300

    feat: add login com google
    adicionado a biblioteca nextauth para fazer o login usando uma conta do google.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,23 +1,41 @@
-import styles from './header.module.css'
-import Link from 'next/link'
+import { useSession, signIn, signOut } from "next-auth/react";
+import styles from "./header.module.css";
+import Link from "next/link";
 
-export function Header(){
-    return(
-        <header className={styles.header}>
-            <section className={styles.content}>
-                <nav className={styles.nav}>
-                    <Link href="/">
-                        <h1 className={styles.logo}>
-                            Tarefas<strong>+</strong>
-                        </h1>
-                    </Link>
-                    <Link href="/dashboard" className={styles.link}>
-                    Meu Painel
-                    </Link>
-                </nav>
+export function Header() {
+  const { data: session, status } = useSession();
 
-                <button className={styles.loginButton}>Acessar</button>
-            </section>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <h1 className={styles.logo}>
+              Tarefas<strong>+</strong>
+            </h1>
+          </Link>
+          {session?.user && (
+            <Link href="/dashboard" className={styles.link}>
+              Meu Painel
+            </Link>
+          )}
+        </nav>
+
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button className={styles.loginButton} onClick={() => signOut()}>
+            Olá {session?.user?.name}
+          </button>
+        ) : (
+          <button
+            className={styles.loginButton}
+            onClick={() => signIn("google")}
+          >
+            Acessar
+          </button>
+        )}
+      </section>
+    </header>
+  );
 }
