1dd76904dae157a4b326da974856ac2ac70d247d
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-08-28 17:38:37 -0300

    styles: página dashboard textarea
    iniciado a estilização da página de dashboard, criação do textarea para criação das tarefas.

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,55 @@
+import { GetServerSideProps } from 'next'
+import styles from './dashboard.module.css'
+import Head from 'next/head'
+import { getSession } from 'next-auth/react'
+import { Textarea } from '@/components/textarea'
+
+export default function Dashboard(){
+    return(
+        <div className={styles.container}>
+            <Head>
+                <title>Meu painel de tarefas</title>
+            </Head>
+
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+                        <form>
+                            <Textarea placeholder='Digite qual sua tarefa'/>
+                            <div className={styles.checkboxArea}>
+                                <input 
+                                    type="checkbox"
+                                    className={styles.checkbox}
+                                />
+                                <label>Deixar tarefa publica?</label>
+                            </div>
+
+                            <button type='submit' className={styles.button}>
+                                Registrar
+                            </button>
+                        </form>
+
+                    </div>
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({ req })
+
+    if(!session?.user){
+        return{
+            redirect:{
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+    return{
+        props: {},
+    }
+}
